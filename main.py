78836d9b3c7002c44aa5defd71695fc271cd0363
AlexanderScorodumov <106822758+AlexanderScorodumov@users.noreply.github.com>
2022-06-16 16:11:27 +0300

    commit main

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import sys
 def solve(a: float, b: float, c: float) -> list[float]:
     if abs(a) < sys.float_info.epsilon:
         raise Exception("Null 'a' argument")
-    if not math.isfinite(a) or not math.isfinite(b) or not math.isfinite(c):
+    if (not math.isfinite(a) 
+        or not math.isfinite(b) 
+        or not math.isfinite(c)):
         raise Exception("Incorrect argument")
     d = b * b - 4 * a * c
+    if abs(d) < sys.float_info.epsilon:
+        return [-b / (2 * a), -b / (2 * a)]
     if d > 0:
         return [(-b + math.sqrt(d)) / (2 * a), (-b - math.sqrt(d)) / (2 * a)]
     if d < 0:
         return []
-    if abs(d) < sys.float_info.epsilon:
-        return [-b / (2 * a), -b / (2 * a)]
--- test/test_main.py
+++ test/test_main.py
@@ -2,33 +2,40 @@ import main
 
 
 def test_solve():
-    #1
-    lst = main.solve(1, 0, -1)
+    #1 Unsolvable
+    lst = main.solve(1, 0, 1)
     assert len(lst) == 0
-    #2
+    #2 2 roots of multiplicity 1
     lst = sorted(main.solve(1, 0, -1))
     assert (lst[0] == -1) and (lst[1] == 1)
-    #3
+    #3 1 root of multiplicity 2
     lst = (main.solve(1, 2, 1))
     assert (lst[0] == -1) and (lst[1] == -1)
-    #4
+    #4 Zero 'a' coefficient
     try:
         lst = main.solve(0, 1, 1)
     except Exception:
         None
     else:
         assert False
-    #5
+    #5 Discriminant less than epsilon
+    lst = (main.solve(1e-8, 1e-8, -1e-9))
+    assert (abs(lst[0] - lst[1]) < main.sys.float_info.epsilon)
+    #6 Non-numeric coefficients
     try:
         lst = main.solve(1, main.math.nan, 1)
     except Exception:
         None
     else:
         assert False
-    #6
-    try:
-        lst = main.solve(1, 1, main.math.inf)
-    except Exception:
-        None
-    else:
-        assert False
+    #6 Non-numeric coefiicients
+    for incorret in main.math.nan, main.math.inf, -main.math.inf:
+        for i in range(3):
+            coef = [1, 1, 1]
+            coef[i] = incorret
+            try:
+                lst = main.solve(coef[0], coef[1], coef[2])
+            except Exception:
+                None
+            else:
+                assert False
