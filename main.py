91143f0c7d06c5158c52738c46d40a80cb46fd49
AlexanderScorodumov <106822758+AlexanderScorodumov@users.noreply.github.com>
2022-06-16 13:35:31 +0300

    Initial commin

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import math
-import os
+import sys
 
 
-def add(a, b) -> int:
-    return math.floor(a + b)
-
-
-def to_sentence(s) -> str:
-    s = s.capitalize()
-
-    if s.endswith('.'):
-        return s
-    else:
-        return s + '.'
+def solve(a: float, b: float, c: float) -> list[float]:
+    if abs(a) < sys.float_info.epsilon:
+        raise Exception("Null 'a' argument")
+    if not math.isfinite(a) or not math.isfinite(b) or not math.isfinite(c):
+        raise Exception("Incorrect argument")
+    d = b * b - 4 * a * c
+    if d > 0:
+        return [(-b + math.sqrt(d)) / (2 * a), (-b - math.sqrt(d)) / (2 * a)]
+    if d < 0:
+        return []
+    if abs(d) < sys.float_info.epsilon:
+        return [-b / (2 * a), -b / (2 * a)]
--- test/test_main.py
+++ test/test_main.py
@@ -1,14 +1,34 @@
 import main
 
 
-def test_add():
-    assert main.add(3, 4) == 7
-    assert main.add(3.5, 4) == 7
-    assert main.add(3.9, 4) == 7
-    assert main.add(3.9, 4.1) == 8
-
-
-def test_to_sentence():
-    assert main.to_sentence('apple') == 'Apple.'
-    assert main.to_sentence('Apple trees') == 'Apple trees.'
-    assert main.to_sentence('Apple trees.') == 'Apple trees.'
+def test_solve():
+    #1
+    lst = main.solve(1, 0, 1)
+    assert len(lst) == 0
+    #2
+    lst = sorted(main.solve(1, 0, -1))
+    assert (lst[0] == -1) and (lst[1] == 1)
+    #3
+    lst = (main.solve(1, 2, 1))
+    assert (lst[0] == -1) and (lst[1] == -1)
+    #4
+    try:
+        lst = main.solve(0, 1, 1)
+    except Exception:
+        None
+    else:
+        assert False
+    #5
+    try:
+        lst = main.solve(1, main.math.nan, 1)
+    except Exception:
+        None
+    else:
+        assert False
+    #6
+    try:
+        lst = main.solve(1, 1, main.math.inf)
+    except Exception:
+        None
+    else:
+        assert False
