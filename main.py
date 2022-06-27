06322258085114246aa05aa8cb1cf32b52347213
AlexanderScorodumov <106822758+AlexanderScorodumov@users.noreply.github.com>
2022-06-27 21:24:51 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,18 +1,86 @@
-import math
-import sys
+from abc import ABC, abstractmethod
+from unittest.mock import Mock
+import numpy as np
 
 
-def solve(a: float, b: float, c: float) -> list[float]:
-    if abs(a) < sys.float_info.epsilon:
-        raise Exception("Null 'a' argument")
-    if (not math.isfinite(a) 
-        or not math.isfinite(b) 
-        or not math.isfinite(c)):
-        raise Exception("Incorrect argument")
-    d = b * b - 4 * a * c
-    if abs(d) < sys.float_info.epsilon:
-        return [-b / (2 * a), -b / (2 * a)]
-    if d > 0:
-        return [(-b + math.sqrt(d)) / (2 * a), (-b - math.sqrt(d)) / (2 * a)]
-    if d < 0:
-        return []
+class Movable(ABC):
+    @abstractmethod
+    def get_position(self) -> list[int]:
+        pass
+
+    @abstractmethod
+    def get_velocity(self) -> list[int]:
+        pass
+
+    @abstractmethod
+    def set_position(self, position: list[int])  -> list[int]:
+        pass
+
+
+class Rotable(ABC):
+    @abstractmethod
+    def get_direction(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_angular_velocity(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_directions_number(self) -> int:
+        pass
+
+    @abstractmethod
+    def set_direction(self, direction: int) -> int:
+        pass
+
+
+class Move:
+    def __init__(self, movable: Movable) -> None:
+        self.movable = movable
+
+    def execute(self) -> list[int]:
+        #position value checking
+        position = self.movable.get_position()
+        if len(position) != 2:
+            raise Exception("Position not correct")
+
+        #velocity value checking
+        velocity = self.movable.get_velocity()
+        if len(velocity) != 2:
+            raise Exception("Velocity not correct")
+
+        #new position calculation
+        new_position = self.movable.set_position(np.ndarray.tolist(np.array(position) + np.array(velocity)))
+        if len(new_position) != 2:
+            raise Exception("New position not correct")
+
+        return new_position
+
+
+class Rotate:
+    def __init__(self, rotable: Rotable) -> None:
+        self.rotable = rotable
+
+    def execute(self) -> int:
+        #direction value checking
+        direction = self.rotable.get_direction()
+        if direction == None:
+            raise Exception("Direction not correct")
+
+        #angular velocity value checking
+        angular_velocity = self.rotable.get_angular_velocity()
+        if angular_velocity == None:
+            raise Exception("Angular velocity not correct")
+
+        #directions number value checking
+        directions_number = self.rotable.get_directions_number()
+        if directions_number == None:
+            raise Exception("Directions number not correct")
+
+        #new direction calculation
+        new_direction = self.rotable.set_direction((direction + angular_velocity) % directions_number)
+        if new_direction == None:
+            raise Exception("New direction not correct")
+
+        return new_direction
--- test/test_main.py
+++ test/test_main.py
@@ -1,41 +1,81 @@
+import unittest
+from unittest.mock import Mock
 import main
 
 
-def test_solve():
-    #1 Unsolvable
-    lst = main.solve(1, 0, 1)
-    assert len(lst) == 0
-    #2 2 roots of multiplicity 1
-    lst = sorted(main.solve(1, 0, -1))
-    assert (lst[0] == -1) and (lst[1] == 1)
-    #3 1 root of multiplicity 2
-    lst = (main.solve(1, 2, 1))
-    assert (lst[0] == -1) and (lst[1] == -1)
-    #4 Zero 'a' coefficient
-    try:
-        lst = main.solve(0, 1, 1)
-    except Exception:
-        None
-    else:
-        assert False
-    #5 Discriminant less than epsilon
-    lst = (main.solve(1e-8, 1e-8, -1e-9))
-    assert (abs(lst[0] - lst[1]) < main.sys.float_info.epsilon)
-    #6 Non-numeric coefficients
-    try:
-        lst = main.solve(1, main.math.nan, 1)
-    except Exception:
-        None
-    else:
-        assert False
-    #6 Non-numeric coefiicients
-    for incorret in main.math.nan, main.math.inf, -main.math.inf:
-        for i in range(3):
-            coef = [1, 1, 1]
-            coef[i] = incorret
-            try:
-                lst = main.solve(coef[0], coef[1], coef[2])
-            except Exception:
-                None
-            else:
-                assert False
+class TestMovable(unittest.TestCase):
+    def test_set_position(self):
+        movable_mock = Mock()
+        attrs = {'get_position.return_value': [12, 5], 'get_velocity.return_value': [-7, 3], 'set_position.return_value': [5, 8]}
+        movable_mock.configure_mock(**attrs)
+        move = main.Move(movable_mock)
+        result = move.execute()
+        expected_result = [5, 8]
+        self.assertListEqual(expected_result, result)
+
+    def test_get_position_exception(self):
+        movable_mock = Mock()
+        attrs = {'get_position.return_value': [], 'get_velocity.return_value': [-7, 3], 'set_position.return_value': [5, 8]}
+        movable_mock.configure_mock(**attrs)
+        move = main.Move(movable_mock)
+        with self.assertRaises(Exception):
+            move.execute()
+
+    def test_get_velocity_exception(self):
+        movable_mock = Mock()
+        attrs = {'get_position.return_value': [12, 5], 'get_velocity.return_value': [], 'set_position.return_value': [5, 8]}
+        movable_mock.configure_mock(**attrs)
+        move = main.Move(movable_mock)
+        with self.assertRaises(Exception):
+            move.execute()
+
+    def test_set_position_exception(self):
+        movable_mock = Mock()
+        attrs = {'get_position.return_value': [12, 5], 'get_velocity.return_value': [-7, 3], 'set_position.return_value': []}
+        movable_mock.configure_mock(**attrs)
+        move = main.Move(movable_mock)
+        with self.assertRaises(Exception):
+            move.execute()
+        
+
+class TestRotable(unittest.TestCase):
+    def test_set_direction(self):
+        rotable_mock = Mock()
+        attrs = {'get_direction.return_value': 5, 'get_angular_velocity.return_value': 11, 'get_directions_number.return_value': 360, 'set_direction.return_value': 16}
+        rotable_mock.configure_mock(**attrs)
+        rotate = main.Rotate(rotable_mock)
+        result = rotate.execute()
+        expected_result = 16
+        self.assertEqual(expected_result, result)
+
+    def test_get_direction_exception(self):
+        rotable_mock = Mock()
+        attrs = {'get_direction.return_value': None, 'get_angular_velocity.return_value': 11, 'get_directions_number.return_value': 360, 'set_direction.return_value': 16}
+        rotable_mock.configure_mock(**attrs)
+        rotate = main.Rotate(rotable_mock)
+        with self.assertRaises(Exception):
+            rotate.execute()
+
+    def test_get_angular_velocity_exception(self):
+        rotable_mock = Mock()
+        attrs = {'get_direction.return_value': 5, 'get_angular_velocity.return_value': None, 'get_directions_number.return_value': 360, 'set_direction.return_value': 16}
+        rotable_mock.configure_mock(**attrs)
+        rotate = main.Rotate(rotable_mock)
+        with self.assertRaises(Exception):
+            rotate.execute()
+
+    def test_get_directions_number_exception(self):
+        rotable_mock = Mock()
+        attrs = {'get_direction.return_value': 5, 'get_angular_velocity.return_value': 11, 'get_directions_number.return_value': None, 'set_direction.return_value': 16}
+        rotable_mock.configure_mock(**attrs)
+        rotate = main.Rotate(rotable_mock)
+        with self.assertRaises(Exception):
+            rotate.execute()
+
+    def test_set_direction_exception(self):
+        rotable_mock = Mock()
+        attrs = {'get_direction.return_value': 5, 'get_angular_velocity.return_value': 11, 'get_directions_number.return_value': 360, 'set_direction.return_value': None}
+        rotable_mock.configure_mock(**attrs)
+        rotate = main.Rotate(rotable_mock)
+        with self.assertRaises(Exception):
+            rotate.execute()
